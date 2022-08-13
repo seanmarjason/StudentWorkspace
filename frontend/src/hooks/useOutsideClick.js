fb9403cab714e952948491b5490e7cd07fa53240
Sean Marjason <sean.marjason@bjss.com>
2022-08-13 10:27:07 +0200

    Fix useOutsideClick hook

--- frontend/src/hooks/useOutsideClick.js
+++ frontend/src/hooks/useOutsideClick.js
@@ -15,8 +15,7 @@ const useOutsideClick = (callback) => {
     return () => {
       document.removeEventListener('click', handleClick, true);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref]);
+  }, [callback, ref]);
 
   return ref;
 };
